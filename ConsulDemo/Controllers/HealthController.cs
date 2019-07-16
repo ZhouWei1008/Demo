e70d594e3ac3060ab560ab9a98626076d36ce0f9
zhouwei1008 <358794405@qq.com>
2019-07-16 23:27:58 +0800

    Update HealthController.cs

--- ConsulDemo/Controllers/HealthController.cs
+++ ConsulDemo/Controllers/HealthController.cs
@@ -7,16 +7,15 @@ using Microsoft.AspNetCore.Mvc;
 using ConsulDemo.Models;
 
 namespace ConsulDemo.Controllers
-{
-    [Produces("application/json")]
-    [Route("api/Health")]
+{     
+    [Route("api/health")]
     public class HealthController : Controller
     {
         [HttpGet]
         public IActionResult Get()
         {
             Console.WriteLine("健康检查" + DateTime.Now);
-            return Ok("ok");
+            return Content("ok");
         }
     }
 }
